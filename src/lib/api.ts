1961bcab43a32c4ba3b15f6de4ad847477528cba
rafaalmeida1 <rafaprof312@gmail.com>
2023-02-25 12:57:35 -0300

    fix: Cors Policy on axios base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://balance-app.cyclic.app/api",
-    // baseURL: "http://localhost:3333/api",
-})
+  baseURL: "https://balance-app.cyclic.app/api",
+//   baseURL: "http://localhost:3333/api",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+  },
+});
