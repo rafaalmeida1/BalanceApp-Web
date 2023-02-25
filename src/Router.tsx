7378767b315bf9f41059d091a95c95e18cbc024b
rafaalmeida1 <rafaprof312@gmail.com>
2023-02-25 14:37:47 -0300

    fix: ROUTER

--- src/Router.tsx
+++ src/Router.tsx
@@ -13,15 +13,19 @@ export function Router() {
       <Route
         path="/user"
         element={
+          <ProtectedRouter>
             <AccountContextProvider>
               <DefaultLayout />
             </AccountContextProvider>
+          </ProtectedRouter>
         }
       >
         <Route
           path=":id"
           element={
+            <ProtectedRouter>
               <Dashboard />
+            </ProtectedRouter>
           }
         />
       </Route>
@@ -31,4 +35,23 @@ export function Router() {
       <Route path="/login" element={<Login />} />
     </Routes>
   );
+}
+
+interface ProtectedRouterProps {
+  children: ReactNode;
+}
+
+export function ProtectedRouter({
+  children,
+}: ProtectedRouterProps): ReactElement | any {
+  const navigate = useNavigate();
+
+  if (
+    localStorage.getItem("@benini-login-auth:1.0.0") ||
+    sessionStorage.getItem("@benini-login-auth:1.0.0")
+  ) {
+    return children;
+  } else {
+    return navigate("/login");
+  }
 }
